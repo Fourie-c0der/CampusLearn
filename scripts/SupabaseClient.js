be0011d92ce0fabf9f38d9e42b814bff2e486577
Oarabile Mbewe <oarabilembewe@gmail.com>
2025-10-22 11:39:22 +0200

    Dynamic home page added

---
+++ scripts/SupabaseClient.js
@@ -0,0 +1,7 @@
+const { createClient } = window.supabase;
+
+// Initialize your Supabase client
+window.supabase = createClient(
+  'https://peexuuzunrhbimpemdwz.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBlZXh1dXp1bnJoYmltcGVtZHd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAxMzk3MjYsImV4cCI6MjA3NTcxNTcyNn0.sOOZfxsQvBF35vijxgO3K5nedKww0fyWKBXiebyfAB0'
+);
